0ff41ad428fa693a9132b6af1a107e06fb15d0cb
omelettebio <omelettebio@proton.me>
2024-09-05 14:32:48 +0200

    debut de la gestion de la grid

---
+++ src/grid/Cell.java
@@ -0,0 +1,11 @@
+package grid;
+
+public class Cell
+{
+    int value; // -1 si pas de valeur et 1 a 9 sinon
+
+    // tableau permettant de determiner ce qui est possible sur cette cellule (vide si aucune valeur)
+    int[] possible_values;
+
+
+}
